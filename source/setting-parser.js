d0f87127e6b8ea7f3da315bf9040d9b9172f563e
Florian Wendelborn <florianwendgithub@gmail.com>
2015-11-13 21:14:52 +0100

    0.0.12 - added a lot of minimap settings

--- source/setting-parser.js
+++ source/setting-parser.js
@@ -50,11 +50,27 @@ SettingParser.prototype.parseGameplay = function () {
 
 	this.parseBoolean(this.gameplay.cameraZoom, 'dota_camera_disable_zoom', true);
 	this.parseBoolean(this.gameplay.cameraMoveOnRespawn, 'dota_reset_camera_on_spawn');
-	this.parseInteger(this.gameplay.cameraSpeed, 'dota_camera_speed');
+	this.parseNumber(this.gameplay.cameraSpeed, 'dota_camera_speed');
+
+	// minimap
+	this.parseNumber(this.gameplay.minimapHeroSize, 'dota_minimap_hero_size');
+	this.parseNumber(this.gameplay.minimapRuneSize, 'dota_minimap_rune_size');
+	this.parseNumber(this.gameplay.minimapCreepScale, 'dota_minimap_creep_scale');
 
 	this.parseBoolean(this.gameplay.minimapProximityScale, 'dota_minimap_hero_scalar');
-	this.parseInteger(this.gameplay.minimapProximityScaleDistance, 'dota_minimap_hero_scalar_distance');
-	this.parseInteger(this.gameplay.minimapProximityScaleMinimum, 'dota_minimap_hero_scalar_minimum');
+	this.parseNumber(this.gameplay.minimapProximityScaleDistance, 'dota_minimap_hero_scalar_distance');
+	this.parseNumber(this.gameplay.minimapProximityScaleMinimum, 'dota_minimap_hero_scalar_minimum');
+
+	this.parseBoolean(this.gameplay.minimapShowHeroIcons, 'dota_minimap_show_hero_icon');
+	this.parseBoolean(this.gameplay.minimapAlwaysShowHeroIcons, 'dota_minimap_always_draw_hero_icons');
+	this.parseBoolean(this.gameplay.minimapBackground, 'dota_minimap_hide_background');
+	this.parseBoolean(this.gameplay.minimapSimpleColors, 'dota_minimap_simple_colors');
+
+	this.parseNumber(this.gameplay.minimapMisclickTime, 'dota_minimap_misclick_time', false, 1000);
+	this.parseBoolean(this.gameplay.minimapRightClick, 'dota_minimap_disable_right_click', true);
+
+	this.parseNumber(this.gameplay.minimapTowerDefendDistance, 'dota_minimap_tower_defend_distance');
+	this.parseNumber(this.gameplay.minimapPingDuration, 'dota_minimap_ping_duration', false, 1000);
 };
 
 SettingParser.prototype.parsePerformance = function () {
@@ -78,22 +94,22 @@ SettingParser.prototype.parsePerformance = function () {
 	this.parseBoolean(this.performance.serverForcePreload, 'sv_forcepreload');
 	this.parseBoolean(this.performance.clientForcePreload, 'cl_forcepreload');
 
-	this.parseInteger(this.performance.shadowQuality, 'cl_globallight_shadow_mode');
-	this.parseInteger(this.performance.levelOfDetail, 'r_lod', 5);
-	this.parseInteger(this.performance.doubleShadowUpdates, 'r_shadow_half_update_rate', true);
-	this.parseInteger(this.performance.maximumFramerate, 'fps_max');
+	this.parseNumber(this.performance.shadowQuality, 'cl_globallight_shadow_mode');
+	this.parseNumber(this.performance.levelOfDetail, 'r_lod', 5);
+	this.parseNumber(this.performance.doubleShadowUpdates, 'r_shadow_half_update_rate', true);
+	this.parseNumber(this.performance.maximumFramerate, 'fps_max');
 
-	this.parseInteger(this.performance.gpuLevel, 'gpu_level');
-	this.parseInteger(this.performance.cpuLevel, 'cpu_level');
-	this.parseInteger(this.performance.gpuMemoryLevel, 'gpu_mem_level');
-	this.parseInteger(this.performance.memoryLevel, 'mem_level');
+	this.parseNumber(this.performance.gpuLevel, 'gpu_level');
+	this.parseNumber(this.performance.cpuLevel, 'cpu_level');
+	this.parseNumber(this.performance.gpuMemoryLevel, 'gpu_mem_level');
+	this.parseNumber(this.performance.memoryLevel, 'mem_level');
 
 	if (this.performance.multiCore !== undefined) {
 		this.append(constants.settings.performanceMultiCoreText);
 		this.parseBoolean(this.performance.multiCore, 'r_threaded_shadow_clip');
 		this.parseBoolean(this.performance.multiCore, 'r_queued_decals');
 		this.parseBoolean(this.performance.multiCore, 'r_queued_post_processing');
-		this.parseInteger(this.performance.multiCore ? 2 : 0, 'mat_que_mode');
+		this.parseNumber(this.performance.multiCore ? 2 : 0, 'mat_que_mode');
 		this.parseBoolean(this.performance.multiCore, 'cl_threaded_bone_setup');
 		this.parseBoolean(this.performance.multiCore, 'cl_threaded_init');
 		this.parseBoolean(this.performance.multiCore, 'snd_mix_async');
@@ -128,10 +144,13 @@ SettingParser.prototype.parseBoolean = function (condition, command, inverse) {
 	}
 };
 
-SettingParser.prototype.parseInteger = function (value, command, inverse) {
+SettingParser.prototype.parseNumber = function (value, command, inverse, unit) {
 	if (value !== undefined && value !== null) {
 		if (inverse) {
 			value = inverse - value;
+		}
+		if (unit) {
+			value /= unit;
 		}
 		this.append(command + ' ' + value);
 	}
